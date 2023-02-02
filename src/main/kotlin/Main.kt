5cabe8ff5589c3c32844eef3f30778daa6c06713
KABANYANA <joyeusekabanyana@gmail.com>
2023-02-02 16:55:10 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var StFirstName = "Joy"
+    var StSecondName = "KABANYANA"
+    var StPhone = 250787520385
+    var StWeight = 75
+    var StAge = 20
+    var StCitizenship = "Rwandan"
+
+    println("the student First name is:" +  StFirstName)
+    println("the student Second name is:" +  StSecondName)
+    println("the student Phone is:" +  StPhone)
+    println("the student Weight is:" +  StWeight)
+    println("the student Age is:" +  StAge)
+    println("the student citizenship is:" +  StCitizenship)
+
+}
