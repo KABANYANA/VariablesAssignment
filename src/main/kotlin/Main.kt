52f5247222c8c8900b7a549d3b085136ae804b5a
KABANYANA <joyeusekabanyana@gmail.com>
2023-02-02 17:21:11 +0300

    added citizenship

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,13 +4,13 @@ fun main(args: Array<String>) {
     var StPhone = 250787520385
     var StWeight = 75
     var StAge = 20
-    var StCitizenship = "Rwandan"
+    var CitizenshipKenyan = true;
 
     println("the student First name is:" +  StFirstName)
     println("the student Second name is:" +  StSecondName)
     println("the student Phone is:" +  StPhone)
     println("the student Weight is:" +  StWeight)
     println("the student Age is:" +  StAge)
-    println("the student citizenship is:" +  StCitizenship)
+    println("is the student citizenship Kenya?:" +  CitizenshipKenyan )
 
 }
